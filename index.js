48aa410168505ecb3c66a07cd8918cd484812c92
KYLE MOLIDOR <kyle_molidor@yahoo.com>
2021-01-07 06:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logshout(string) {
+function logShout(string) {
   console.log(string.toUpperCase())
 }
-function logwhisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
